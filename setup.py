9c8838b176de0bf5da8f860f677d30fa55d2557a
caglarcakan <sevensirk@gmail.com>
2020-01-26 14:32:35 +0100

    testing tests

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ import setuptools
 
 from os import path
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README_pypi.md'), encoding='utf-8') as f:
+#with open(path.join(this_directory, 'README_pypi.md'), encoding='utf-8') as f:
+with open('README_pypi.md', encoding='utf-8') as f:
     long_description = f.read()
 
 with open('requirements.txt') as f:
