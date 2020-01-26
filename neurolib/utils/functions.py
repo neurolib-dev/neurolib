b1b35b733868ee9d3d7564565b9f267740bf72eb
caglarcakan <sevensirk@gmail.com>
2020-01-26 08:01:51 +0100

    added docstrings

--- neurolib/utils/functions.py
+++ neurolib/utils/functions.py
@@ -4,14 +4,16 @@ import numpy as np
 
 def analyse_run(measure = 'domfr', result = [], dt = 0.1):
     '''
-    Analysis routine for bifurcation diagrams with a 'rect' stimulus that is used
-    to detect bistability.
+    Analysis routine for bifurcation diagrams in Cakan2020 
+    with a 'rect' stimulus that is used to detect bistability.
     
-    measure:    Pass the measure you want to compute in string form, such as "domfr_power_exc"
-    result:     Timeseries of a successful simulation.
-    dt:         Integration timestep of simulations in ms
-
+    Parameters
+    ----------
+        :param measure:    Pass the measure you want to compute in string form, such as "domfr_power_exc"
+        :param result:     Timeseries of a successful simulation.
+        :param dt:         Integration timestep of simulations in ms
     '''
+
     t = result['t']
     
     down_window = (2000<t) & (t<3000) # time period in ms where we expect the down-state
@@ -81,7 +83,30 @@ def analyse_run(measure = 'domfr', result = [], dt = 0.1):
             Pxx_spec /= np.max(Pxx_spec)
             return f, Pxx_spec
 
-def kuramoto(traces, smoothing=0.0, dt = 0.1, debug = False, peakrange=[0.1, 0.2]):
+def kuramoto(traces, dt = 0.1, smoothing=0.0, peakrange=[0.1, 0.2]):
+    """
+    Computes the Kuramoto order parameter of a timeseries. 
+    Can smooth timeseries if there is noise. Peaks are then detected using a peakfinder.
+    From these peaks a phase is derived and then phase synchrony (Kuramoto order parameter)
+    is computed across all timeseries. 
+    
+    Parameters
+    ----------
+        traces : numpy array
+            Multidimensional timeseries array
+        dt : float
+            Integration timestep
+        smoothing : float, optional
+            Gaussian smoothing strength
+        peakrange : list of floats, length 2
+            Width range of peaks for peak detection
+    
+    Returns
+    -------
+        kuramoto : numpy array
+            Timeseries of Kuramoto order paramter
+    """
+
     phases = []
     nTraces = len(traces)
     for n in range(nTraces):
@@ -99,6 +124,7 @@ def kuramoto(traces, smoothing=0.0, dt = 0.1, debug = False, peakrange=[0.1, 0.2
             lastMax = 0
             for m in maximalist:
                 for t in range(lastMax, m):
+                    # compute instantaneous phase
                     phi = 2 * np.pi * float(t - lastMax) / float(m - lastMax)
                     phases[n].append(phi)
                 lastMax = m
@@ -119,24 +145,76 @@ def kuramoto(traces, smoothing=0.0, dt = 0.1, debug = False, peakrange=[0.1, 0.2
     return kuramoto
 
 def matrix_correlation(M1, M2):
-    return np.corrcoef(M1.reshape((1,M1.size)), M2.reshape( (1,M2.size) ) )[0,1]
-    
-def fc(BOLD):
-    simFC = np.corrcoef(BOLD)
-    simFC = np.nan_to_num(simFC) # remove NaNs
-    return simFC
+    """
+    Pearson correlation of the lower triagonal of two matrices.
+    The triangular matrix is offset by k = 1 in order to ignore the diagonal line
 
-def fcd(BOLD, windowsize = 30, stepsize = 5, N=90):
-    # compute FCD matrix
-    t_window_width = int(windowsize)# int(windowsize * 30) # x minutes
-    stepsize = stepsize # BOLD.shape[1]/N
+    Parameters
+    ----------
+        M1 : numpy array
+            N x N matrix
+        M2 : numpy array
+            N x N matrix, must have same dimensions as M1
+
+    Returns
+    -------
+        cc : float
+            Correlation coefficient    
+
+    """
+
+    cc = np.corrcoef(M1[np.triu_indices_from(M1, k=1)], M2[np.triu_indices_from(M2, k=1)])[0,1]
+    return cc
+    
+def fc(ts):
+    """
+    Functional connectivity matrix of timeseries multidimensional `ts` (Nxt).
+    Pearson correlation (from `np.corrcoef()` is used).
+    
+    Parameters
+    ----------
+        ts : numpy array
+            Nxt timeseries
+    
+    Returns
+    -------
+        fc : numpy array
+            N x N functional connectivity matrix
+    """
+
+    fc = np.corrcoef(ts)
+    fc = np.nan_to_num(fc) # remove NaNs
+    return fc
+
+def fcd(ts, windowsize = 30, stepsize = 5):
+    """
+    Computes FCD (functional connectivity dynamics) matrix, as described in Deco's whole-brain model papers.
+    Default paramters are adjusted for BOLD timeseries: windowsize = 30 (=60s) and stepsize = 5 (=10s).
+
+    Parameters
+    ----------
+        ts : numpy array
+            Nxt timeseries
+        windowsize : int
+            Size of each rolling window in timesteps
+        stepsize : int
+            Stepsize between each rolling window
+    
+    Returns
+    -------
+        fc : numpy array
+            T x T FCD matrix    
+    """
+
+    t_window_width = int(windowsize) # int(windowsize * 30) # x minutes
+    stepsize = stepsize # ts.shape[1]/N
     corrFCs = []
     try:
-        counter = range(0, BOLD.shape[1]-t_window_width, stepsize)
+        counter = range(0, ts.shape[1]-t_window_width, stepsize)
 
         for t in counter:
-            BOLD_slice = BOLD[:,t:t+t_window_width]
-            corrFCs.append(np.corrcoef(BOLD_slice))
+            ts_slice = ts[:,t:t+t_window_width]
+            corrFCs.append(np.corrcoef(ts_slice))
 
         FCd = np.empty([len(corrFCs),len(corrFCs)])
         f1i = 0
@@ -151,36 +229,10 @@ def fcd(BOLD, windowsize = 30, stepsize = 5, N=90):
     except:
         return 0
 
-def dom_freqs(ts, one_over_dt = 2e3, freqcut = 300, welch_param = 4*1024):
-    # return dominant frequencies of a timeseries    
-    dominant_freqs = []
-    if len(ts.shape)>1: # multiple nodes
-        for n in range(len(ts)): #range(33,45):
-            thisdata = ts[n, :]
-            f, Pxx_spec = scipy.signal.welch(thisdata, one_over_dt, 'flattop', welch_param, scaling='spectrum')
-            # find dominant frequencies
-            f = f[f<freqcut]
-            y_av = np.sqrt(Pxx_spec)
-            x = f
-            max_y = max(y_av)  # Find the maximum y value
-            max_x = x[y_av.argmax()]  # Find the x value corresponding to the maximum y value
-            dominant_freqs.append(max_x)
-    else: # single node
-        thisdata = ts
-        f, Pxx_spec = scipy.signal.welch(thisdata, one_over_dt, 'flattop', welch_param, scaling='spectrum')
-        # frequency cut
-        f = f[f<freqcut]
-        Pxx_spec = Pxx_spec[0:len(f)]
-        # find dominant frequencies
-        y_av = Pxx_spec
-        x = f
-        max_y = max(y_av)  # Find the maximum y value
-        max_x = x[y_av.argmax()]  # Find the x value corresponding to the maximum y value
-        dominant_freqs = max_x
-    return dominant_freqs
-
 def kolmogorov(BOLD1, BOLD2, windowsize = 1.0):
-    # return kolmogorov distance between two functional connectivities
+    """
+    Computes kolmogorov distance between two FCS matrices.
+    """
     empiricalFCD = fcd(BOLD2[:,:len(BOLD1[0,:])], windowsize = windowsize)
     FCD = fcd(BOLD1[:, 10:], windowsize = windowsize)
     
@@ -190,17 +242,40 @@ def kolmogorov(BOLD1, BOLD2, windowsize = 1.0):
     emptriUFCD = np.triu(empiricalFCD)
     emptriUFCD = emptriUFCD[(emptriUFCD>0.0)&(emptriUFCD<1.0)]
 
-    
-
     return scipy.stats.ks_2samp(triUFCD, emptriUFCD)[0]
 
 def print_params(params):
-    # helper function to print the current set of parameters
+    """
+    Helpfer function to print the current set of parameters.
+    """
     paramsOfInterest = ['dt', 'Ke_gl', 'mue_ext_mean', 'mui_ext_mean', 'sigma_ou', 'signalV', 'a', 'b', 'Jee_max', 'Jie_max', 'Jii_max', 'Jei_max', 'cee', 'cie', 'cii', 'cei', 'Ke', 'Ki', 'de', 'di']
     for p in paramsOfInterest:
         print("params[\'%s\'] = %0.3f"%(p, params[p]))
 
 def getPowerSpectrum(rate, dt, maxfr = 70, spectrum_windowsize = 1.0, normalize = False):
+    """
+    Returns a power spectrum using Welch's method.
+
+    Parameters
+    ----------
+        rate : numpy array
+            One-dimensional timeseries
+        dt : float
+            Simulation time step
+        maxfr : float, optional
+            Maximum frequency to cutoff from return
+        spectrum_windowsize : float
+            Length of the window used in Welch's method (in seconds)
+        normalize : bool
+            Maximum power is normalized to 1 if True
+    
+    Returns
+    -------
+        f : list
+            Frequencies
+        pwers : list
+            Powers
+    """
     f, Pxx_spec =  scipy.signal.welch(
             rate,
             1000/dt,
@@ -214,6 +289,9 @@ def getPowerSpectrum(rate, dt, maxfr = 70, spectrum_windowsize = 1.0, normalize 
     return f, Pxx_spec
 
 def getMeanPowerSpectrum(rates, dt, maxfr = 70, spectrum_windowsize = 1.0, normalize = False):
+    """
+    Returns the mean power spectrum of multiple timeseries.
+    """    
     powers = np.zeros(getPowerSpectrum(rates[0], dt, maxfr, spectrum_windowsize)[0].shape)
     ps = []
     for rate in rates:
