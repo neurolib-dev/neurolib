2c3a29d76d747b383814e6d56a6ebc2ee82432eb
caglarcakan <sevensirk@gmail.com>
2020-01-26 14:52:30 +0100

    testing tests

--- setup.py
+++ setup.py
@@ -22,6 +22,6 @@ setuptools.setup(name='neurolib',
                 "Programming Language :: Python :: 3",
                 "License :: OSI Approved :: MIT License",
                 "Operating System :: OS Independent"],
-        python_requires='>=3.6',
+        python_requires='>=3.7',
         install_requires = requirements,
         include_package_data=True)
