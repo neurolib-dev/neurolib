3847f2db7ffa55522a5213e0712790df5ca79668
caglarcakan <sevensirk@gmail.com>
2020-01-26 15:06:57 +0100

    verbose test

--- tests/test.py
+++ tests/test.py
@@ -1,3 +1,6 @@
+print("Running tests ...")
+# ----------------------------------------
+print("\t > Testing single node ...")
 from neurolib.models import aln
 
 alnModel = aln.ALNModel()
@@ -5,6 +8,8 @@ alnModel.params['sigma_ou'] = 0.1 # add some noise
 
 alnModel.run()
 
+# ----------------------------------------
+print("\t > Testing brain network ...")
 from neurolib.utils.loadData import Dataset
 
 ds = Dataset("gw")
@@ -13,3 +18,5 @@ alnModel = aln.ALNModel(Cmat = ds.Cmat, Dmat = ds.Dmat, simulateBOLD=True)
 alnModel.params['duration'] = 0.5*60*1000 # in ms, simulates for 5 minutes
 
 alnModel.run()
+# ----------------------------------------
+print("All tests passed!")
