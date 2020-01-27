8d4b309d88e6f4b63b66fc67c344239eea36f4ef
Nikola Jajcay <nikola.jajcay@gmail.com>
2020-01-27 23:47:05 +0100

    tests for connectivity helpers and friends

--- tests/models/test_models.py
+++ tests/models/test_models.py
@@ -1,5 +1,3 @@
-import logging
-import time
 import unittest
 
 from neurolib.models import aln, hopf
@@ -12,30 +10,22 @@ class TestALNModel(unittest.TestCase):
     """
 
     def test_single_node(self):
-        logging.info("\t > ALN: Testing single node ...")
-        start = time.time()
-
         alnModel = aln.ALNModel()
         alnModel.params["duration"] = 2.0 * 1000
         alnModel.params["sigma_ou"] = 0.1  # add some noise
 
         alnModel.run()
-        end = time.time()
-        logging.info("\t > Done in {:.2f} s".format(end - start))
 
     def test_network(self):
         logging.info("\t > ALN: Testing brain network (chunkwise integration and BOLD" " simulation) ...")
         start = time.time()
 
         ds = Dataset("gw")
-
         alnModel = aln.ALNModel(Cmat=ds.Cmat, Dmat=ds.Dmat, simulateBOLD=True)
         # in ms, simulates for 5 minutes
         alnModel.params["duration"] = 10 * 1000
 
         alnModel.run()
-        end = time.time()
-        logging.info("\t > Done in {:.2f} s".format(end - start))
 
 
 class TestHopfModel(unittest.TestCase):
@@ -45,14 +35,11 @@ class TestHopfModel(unittest.TestCase):
 
     def test_single_node(self):
         logging.info("\t > Hopf: Testing single node ...")
-        start = time.time()
         hopfModel = hopf.HopfModel()
         hopfModel.params["duration"] = 2.0 * 1000
         hopfModel.params["sigma_ou"] = 0.03
 
         hopfModel.run()
-        end = time.time()
-        logging.info("\t > Done in {:.2f} s".format(end - start))
 
     def test_network(self):
         logging.info("\t > Hopf: Testing brain network (chunkwise integration and BOLD" " simulation) ...")
@@ -66,8 +53,6 @@ class TestHopfModel(unittest.TestCase):
         hopfModel.params["K_gl"] = 0.6
 
         hopfModel.run()
-        end = time.time()
-        logging.info("\t > Done in {:.2f} s".format(end - start))
 
 
 if __name__ == "__main__":
