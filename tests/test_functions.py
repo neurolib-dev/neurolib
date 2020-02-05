efd12fec27f973cedf7a9a6da17744f8f26ff17a
caglarcakan <sevensirk@gmail.com>
2020-02-05 18:02:35 +0100

    test: function testing

---
+++ tests/test_functions.py
@@ -0,0 +1,51 @@
+import logging
+import time
+import unittest
+
+import neurolib.utils.functions as func
+from neurolib.models.aln import ALNModel
+from neurolib.utils.loadData import Dataset
+
+
+class TestFunctions(unittest.TestCase):
+    """
+    Test functions in neurolib/utils/functions.py
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        ds = Dataset("gw")
+        aln = ALNModel(Cmat=ds.Cmat, Dmat=ds.Dmat, simulateBOLD=True, chunkSize=500)
+
+        # Resting state fits
+        aln.params["mue_ext_mean"] = 1.57
+        aln.params["mui_ext_mean"] = 1.6
+        aln.params["sigma_ou"] = 0.09
+        aln.params["b"] = 5.0
+        aln.params["duration"] = 0.2 * 60 * 1000
+        aln.run()
+
+        cls.model = aln
+
+    def test_kuramoto(self):
+        kuramoto = func.kuramoto(self.model.rates_exc, dt=self.model.params["dt"], smoothing=5.0)
+
+    def test_fc(self):
+        FC = func.fc(self.model.BOLD.BOLD)
+
+    def test_fcd(self):
+        rFCD = func.fcd(self.model.rates_exc, stepsize=100)
+
+    def test_matrix_correlation(self):
+        FC = func.fc(self.model.BOLD.BOLD)
+        cc = func.matrix_correlation(FC, ds.FCs[0])
+
+    def test_print_params(self):
+        func.print_params(self.model.params)
+
+    def test_getPowerSpectrum(self):
+        fr, pw = func.getPowerSpectrum(self.model.rates_exc[0, :], dt=self.model.params["dt"])
+
+    def test_getMeanPowerSpectrum(self):
+        fr, pw = func.getMeanPowerSpectrum(self.model.rates_exc, dt=self.model.params["dt"])
+
