82add32a234476412dc9f4ade79e0b2ff87d5558
caglarcakan <sevensirk@gmail.com>
2020-02-06 18:02:54 +0100

    testing: use unittest asserts

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -11,17 +11,17 @@ import neurolib.utils.functions as func
 class TestDatasets(unittest.TestCase):
     def test_dataset_gw(self):
         ds = Dataset("gw")
-        assert ds.Cmat.shape == (80, 80)
-        assert ds.Dmat.shape == (80, 80)
-        assert ds.FCs[0].shape == (80, 80)
-        assert len(ds.FCs) == len(ds.BOLDs)
+        self.assertTupleEqual(ds.Cmat.shape, (80, 80))
+        self.assertTupleEqual(ds.Dmat.shape, (80, 80))
+        self.assertTupleEqual(ds.FCs[0].shape, (80, 80))
+        self.assertTrue(len(ds.FCs) == len(ds.BOLDs))
 
     def test_dataset_hcp(self):
         ds = Dataset("hcp")
-        assert ds.Cmat.shape == (80, 80)
-        assert ds.Dmat.shape == (80, 80)
-        assert ds.FCs[0].shape == (80, 80)
-        assert len(ds.FCs) == len(ds.BOLDs)
+        self.assertTupleEqual(ds.Cmat.shape, (80, 80))
+        self.assertTupleEqual(ds.Dmat.shape, (80, 80))
+        self.assertTupleEqual(ds.FCs[0].shape, (80, 80))
+        self.assertTrue(len(ds.FCs) == len(ds.BOLDs))
 
     def test_manual_loading(self):
         dsBaseDirectory = os.path.join("neurolib", "data", "datasets", "gw")
