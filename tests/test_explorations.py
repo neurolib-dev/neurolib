e18ed48d945f8ddcb649a75840aa14278af16b27
caglarcakan <sevensirk@gmail.com>
2020-02-12 17:31:58 +0100

    testing: whole brain network

--- tests/test_explorations.py
+++ tests/test_explorations.py
@@ -21,7 +21,6 @@ class TestALNExploration(unittest.TestCase):
         aln = ALNModel()
         parameters = ParameterSpace({"mue_ext_mean": np.linspace(0, 3, 2), "mui_ext_mean": np.linspace(0, 3, 2)})
         search = BoxSearch(aln, parameters)
-        search.initializeExploration()
         search.run()
         search.loadResults()
 
@@ -32,6 +31,108 @@ class TestALNExploration(unittest.TestCase):
 
         end = time.time()
         logging.info("\t > Done in {:.2f} s".format(end - start))
+
+    def test_brain_network(self):
+        from neurolib.utils.loadData import Dataset
+
+        ds = Dataset("hcp")
+        aln = ALNModel(
+            Cmat=ds.Cmat, Dmat=ds.Dmat, simulateBOLD=True
+        )  # simulates the whole-brain model in 10s chunks by default if simulateBOLD == True
+        # Resting state fits
+        aln.params["mue_ext_mean"] = 1.57
+        aln.params["mui_ext_mean"] = 1.6
+        aln.params["sigma_ou"] = 0.09
+        aln.params["b"] = 5.0
+        aln.params["signalV"] = 2
+        aln.params["dt"] = 0.2
+        aln.params["duration"] = 0.2 * 60 * 1000
+
+        # multi stage evaluation function
+        def evaluateSimulation(traj):
+            print("asd")
+            print("I'm hereee")
+            model = search.getModelFromTraj(traj)
+            defaultDuration = model.params["duration"]
+            invalid_result = {"fc": [0] * len(ds.BOLDs)}
+
+            # -------- stage wise simulation --------
+
+            # Stage 1 : simulate for a few seconds to see if there is any activity
+            # ---------------------------------------
+            model.params["dt"] = 0.1
+            model.params["duration"] = 3 * 1000.0
+            model.simulateBOLD = False
+            model.run()
+
+            # check if stage 1 was successful
+            if np.max(aln.rates_exc[:, aln.t > 500]) > 300 or np.max(aln.rates_exc[:, aln.t > 500]) < 10:
+                search.saveOutputsToPypet(invalid_result, traj)
+                return invalid_result, {}
+
+            # Stage 2: simulate BOLD for a few seconds to see if it moves
+            # ---------------------------------------
+            model.params["dt"] = 0.2
+            model.params["duration"] = 20 * 1000.0
+            model.simulateBOLD = True
+            model.run()
+
+            if np.std(aln.BOLD.BOLD[:, 5:10]) < 0.001:
+                search.saveOutputsToPypet(invalid_result, traj)
+                return invalid_result, {}
+
+            # Stage 3: full and final simulation
+            # ---------------------------------------
+            model.params["dt"] = 0.2
+            model.params["duration"] = defaultDuration
+            model.run()
+
+            # -------- evaluation here --------
+
+            scores = []
+            for i, fc in enumerate(ds.FCs):  # range(len(ds.FCs)):
+                fc_score = func.matrix_correlation(func.fc(model.BOLD.BOLD[:, 5:]), fc)
+                scores.append(fc_score)
+
+            meanScore = np.mean(scores)
+            result_dict = {"fc": meanScore}
+
+            search.saveOutputsToPypet(result_dict, traj)
+
+        # define and run exploration
+        parameters = ParameterSpace({"mue_ext_mean": np.linspace(0, 3, 2), "mui_ext_mean": np.linspace(0, 3, 2)})
+        search = BoxSearch(evalFunction=evaluateSimulation, model=aln, exploreParameters=parameters)
+        search.run()
+        # postprocessing
+        # search.loadResults()
+        # print("Number of results: {}".format(len(search.results)))
+        # self.assertTrue(len(search.results) > 0)
+        # for i in search.dfResults.index:
+        #     search.dfResults.loc[i, "fc"] = np.mean(search.results[i]["fc"])
+        # search.dfResults
+
+
+class TestCustomParameterExploration(unittest.TestCase):
+    """Exploration with custom function
+    """
+
+    def test_circle_exploration(self):
+        def explore_me(traj):
+            pars = search.getParametersFromTraj(traj)
+            # let's calculate the distance to a circle
+            computation_result = abs((pars["x"] ** 2 + pars["y"] ** 2) - 1)
+            result_dict = {"distance": computation_result}
+            search.saveOutputsToPypet(result_dict, traj)
+
+        parameters = ParameterSpace({"x": np.linspace(-2, 2, 2), "y": np.linspace(-2, 2, 2)})
+        search = BoxSearch(evalFunction=explore_me, exploreParameters=parameters)
+        search.run()
+        search.loadResults()
+
+        for i in search.dfResults.index:
+            search.dfResults.loc[i, "distance"] = search.results[i]["distance"]
+
+        search.dfResults
 
 
 if __name__ == "__main__":
