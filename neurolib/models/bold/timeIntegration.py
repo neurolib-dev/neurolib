6e0ad1c0d8a50acf8500b3df56e77b01ac6dbf93
caglarcakan <sevensirk@gmail.com>
2020-02-13 19:33:17 +0100

    bold: clean dosctrings and comments

--- neurolib/models/bold/timeIntegration.py
+++ neurolib/models/bold/timeIntegration.py
@@ -1,27 +1,30 @@
 import numpy as np
-
 import numba
 
 
 def simulateBOLD(Z, dt, voxelCounts, X=None, F=None, Q=None, V=None):
-    # PREDICTEDBOLD Compute the simulated BOLD signal out of the synaptic activity using the Ballon-Windkessel model
-    #   BOLD = predictBold(synapticActivity,dt,voxelCounts)
-    #
-    #   Params:
-    #       synapticActivity    N x length matrix. Synaptic activity of the Nth brain area at each time steps (Hz)
-    #       dt                  Scalar. Sampling period for the synaptic activity. (s)
-    #                           length x dt = simulation duration
-    #       voxelCounts         (optional) N x 1 matrix. Number of fMRI voxels contained in each brain area.
-    #       X                   (optional) N x 1 matrix. Initial values: Vasso dilatory signal
-    #       F                   (optional) N x 1 matrix. Initial values: Blood flow
-    #       Q                   (optional) N x 1 matrix. Initial values: Deoxyhemoglobin
-    #       V                   (optional) N x 1 matrix. Initial values: Blood volume
-    #
-    #   Return:
-    #       BOLD    N x length matrix: simulated BOLD for the N brain areas.
-    #               Note: This signal needs then to be downsampled to be compared to the empirical fMRI signal
-    #
-    #   See Friston 2003 and Deco 2013 for reference on how the BOLD signal is simulated
+    """Simulate BOLD activity using the Balloon-Windkessel model.
+    See Friston 2003 and Deco 2013 for reference on how the BOLD signal is simulated.
+    The returned BOLD signal should be downsampled to be comparable to a recorded fMRI signal.
+
+    :param Z: Synaptic activity
+    :type Z: numpy.ndarray
+    :param dt: dt of input activity in s
+    :type dt: float
+    :param voxelCounts: Number of voxels in each region (not used yet!)
+    :type voxelCounts: numpy.ndarray
+    :param X: Initial values of Vasodilatory signal, defaults to None
+    :type X: numpy.ndarray, optional
+    :param F: Initial values of Blood flow, defaults to None
+    :type F: numpy.ndarray, optional
+    :param Q: Initial values of Deoxyhemoglobin, defaults to None
+    :type Q: numpy.ndarray, optional
+    :param V: Initial values of Blood volume, defaults to None
+    :type V: numpy.ndarray, optional
+
+    :return: BOLD, X, F, Q, V
+    :rtype: (numpy.ndarray,)
+    """
 
     N = np.shape(Z)[0]
 
@@ -31,9 +34,10 @@ def simulateBOLD(Z, dt, voxelCounts, X=None, F=None, Q=None, V=None):
     voxelCountsSqrtInv = 1 / np.sqrt(voxelCounts)
 
     # Balloon-Windkessel model parameters (Deco 2013, Friston 2003):
-    # Note: the distribution of each Balloon-Windkessel models parameters values are given pro Voxels
-    #   Since we average the empirical fMRI of each voxel for a given area, the standard deviation of the gaussian distribution
-    #   should be divided by the number of voxels present in the area
+    # Note: the distribution of each Balloon-Windkessel models parameters are given per voxel
+    # Since we usually average the empirical fMRI of each voxel for a given area, the standard
+    # deviation of the gaussian distribution should be divided by the number of voxels in each area
+    #
     # rho     = np.random.normal(0.34, np.sqrt(0.0024) / np.sqrt( sum(voxelCounts) ) )    # Capillary resting net oxygen extraction
     # alpha   = np.random.normal(0.32, np.sqrt(0.0015) / np.sqrt( sum(voxelCounts) ) )    # Grubb's vessel stiffness exponent
     # V0      = 0.02
@@ -43,6 +47,8 @@ def simulateBOLD(Z, dt, voxelCounts, X=None, F=None, Q=None, V=None):
     # Gamma   = np.random.normal(0.41 * np.ones(N), np.sqrt(0.002) * voxelCountsSqrtInv)   # Rate constant for autoregulatory feedback by blood flow
     # K       = np.random.normal(0.65 * np.ones(N), np.sqrt(0.015) * voxelCountsSqrtInv)   # Vasodilatory signal decay
     # Tau     = np.random.normal(0.98 * np.ones(N), np.sqrt(0.0568) * voxelCountsSqrtInv)   # Transit time
+    #
+    # If no voxel counts are given, we can use scalar values for each region's parameter:
     rho = 0.34  # Capillary resting net oxygen extraction (dimensionless)
     alpha = 0.32  # Grubb's vessel stiffness exponent (dimensionless)
     V0 = 0.02  # Resting blood volume fraction (dimensionless)
@@ -53,8 +59,7 @@ def simulateBOLD(Z, dt, voxelCounts, X=None, F=None, Q=None, V=None):
     K = 0.65 * np.ones((N,))  # Vasodilatory signal decay (1/s)
     Tau = 0.98 * np.ones((N,))  # Transit time  (s)
 
-    # ballon-Windkessel state variables
-
+    # initialize state variables
     if X is None:
         X = np.zeros((N,))  # Vasso dilatory signal
     if F is None:
@@ -71,17 +76,9 @@ def simulateBOLD(Z, dt, voxelCounts, X=None, F=None, Q=None, V=None):
 
 @numba.njit
 def integrateBOLD_numba(BOLD, X, Q, F, V, Z, dt, N, rho, alpha, V0, k1, k2, k3, Gamma, K, Tau):
-
+    """Integrate the Balloon-Windkessel model
+    """
     for i in range(len(Z[0, :])):  # loop over all timesteps
-
-        #        X = X + dt * ( Z[:,i] - K * X - Gamma * ( F - 1 ) )
-        #        Q = Q + dt / Tau * ( F / rho * ( 1 - ( 1 - rho ) ** (1 / F) )  \
-        #                             - Q * V ** ( 1 / alpha - 1 ) )
-        #        V = V + dt / Tau * ( F - V ** ( 1 / alpha ) )
-        #        F = F + dt * X
-        #
-        #        BOLD[:,i] = V0 * ( k1 * ( 1 - Q) + k2 * (1 - Q / V ) + k3 * (1 - V) )
-
         # component-wise loop for compatibilty with numba
         for j in range(N):  # loop over all areas
             X[j] = X[j] + dt * (Z[j, i] - K[j] * X[j] - Gamma[j] * (F[j] - 1))
