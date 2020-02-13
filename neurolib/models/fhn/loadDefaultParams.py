6d48bbbbbc843ec4d2c8aeba9580e6c7a8546a2f
caglarcakan <sevensirk@gmail.com>
2020-02-13 15:57:43 +0100

    fhn: model implemented

---
+++ neurolib/models/fhn/loadDefaultParams.py
@@ -0,0 +1,94 @@
+import numpy as np
+
+import h5py
+import scipy.io
+
+
+def loadDefaultParams(Cmat=[], Dmat=[], seed=None):
+    """
+    Load default parameters for the for a whole network of aLN nodes.
+    A lot of the code which deals with loading structural connectivity
+    and delay matrices for an interareal whole-brain network simulation
+    can be ignored if only a single E-I module is to be simulated (set singleNode=1 then)
+
+    Parameters:
+        :param lookUpTableFileName:     Matlab filename where to find the lookup table. Default: interarea/networks/aln-python/aln-precalc/precalc05sps_all.mat'
+    :returns:   A dictionary of default parameters
+    """
+
+    class struct(object):
+        pass
+
+    params = struct()
+
+    ### runtime parameters
+    params.dt = 0.1  # ms 0.1ms is reasonable
+    params.duration = 2000  # Simulation duration (ms)
+    params.seed = np.int64(0)  # seed for RNG of noise and ICs
+
+    # ------------------------------------------------------------------------
+    # global whole-brain network parameters
+    # ------------------------------------------------------------------------
+
+    params.signalV = 20.0
+    params.K_gl = 250.0  # global coupling strength
+
+    if len(Cmat) == 0:
+        params.N = 1
+        params.Cmat = np.zeros((1, 1))
+        params.lengthMat = np.zeros((1, 1))
+
+    else:
+        params.Cmat = Cmat.copy()  # coupling matrix
+        np.fill_diagonal(Cmat, 0)  # no self connections
+        params.Cmat = Cmat / np.max(Cmat)  # normalize matrix
+        params.N = len(params.Cmat)  # number of nodes
+        params.lengthMat = Dmat
+
+    # ------------------------------------------------------------------------
+    # local node parameters
+    # ------------------------------------------------------------------------
+
+    # external input parameters:
+    params.I = 0.4
+
+    # noise parameters
+    params.tau_ou = 5.0  # ms Timescale of the Ornstein-Uhlenbeck noise process
+    params.sigma_ou = 0.0  # mV/ms/sqrt(ms) noise intensity
+    params.x_ext_mean = 0.0  # mV/ms (OU process) [0-5]
+    params.y_ext_mean = 0.0  # mV/ms (OU process) [0-5]
+
+    # neural mass model parameters
+    params.alpha = 3.0  # Eqpsilon in Kostova et al. (2005)
+    params.beta = 4.0  # eps(1+lam)
+    params.gamma = -1.5  # lam eps
+    params.delta = 0.0
+    params.epsilon = 0.5  # a
+    params.tau = 20.0
+    # ------------------------------------------------------------------------
+
+    params.xs_init = 0.05 * np.random.uniform(0, 1, (params.N, 1))
+    params.ys_init = 0.05 * np.random.uniform(0, 1, (params.N, 1))
+
+    params_dict = params.__dict__
+
+    return params_dict
+
+
+def computeDelayMatrix(lengthMat, signalV, segmentLength=1):
+    """Compute the delay matrix from the fiber length matrix and the signal velocity
+
+        :param lengthMat:       A matrix containing the connection length in segment
+        :param signalV:         Signal velocity in m/s
+        :param segmentLength:   Length of a single segment in mm
+
+        :returns:    A matrix of connexion delay in ms
+    """
+
+    normalizedLenMat = lengthMat * segmentLength
+    # Interareal connection delays, Dmat(i,j) in ms
+    if signalV > 0:
+        Dmat = normalizedLenMat / signalV
+    else:
+        Dmat = lengthMat * 0.0
+    return Dmat
---
+++ neurolib/models/fhn/model.py
@@ -0,0 +1,78 @@
+import neurolib.models.fhn.chunkwiseIntegration as cw
+import neurolib.models.fhn.loadDefaultParams as dp
+import neurolib.models.fhn.timeIntegration as ti
+from neurolib.models.model import Model
+
+
+class FHNModel(Model):
+    """
+    Todo.
+    """
+
+    name = "fhn"
+    description = "Fitz-Hugh Nagumo oscillator"
+
+    modelOutputs = {"activity": ["x", "y"]}
+
+    defaultOutput = "x"
+
+    def __init__(
+        self,
+        params=None,
+        Cmat=[],
+        Dmat=[],
+        lookupTableFileName=None,
+        seed=None,
+        simulateChunkwise=False,
+        chunkSize=10000,
+        simulateBOLD=False,
+    ):
+        # Initialize base class Model
+        super().__init__(self.name)
+
+        if len(Cmat) == 0:
+            self.singleNode = True
+        else:
+            self.singleNode = False
+        self.Cmat = Cmat
+        self.Dmat = Dmat
+        self.seed = seed
+
+        self.simulateChunkwise = simulateChunkwise
+        self.chunkSize = chunkSize  # Size of integration chunks in chunkwise integration
+        self.simulateBOLD = simulateBOLD  # BOLD
+        if simulateBOLD:
+            self.simulateChunkwise = True  # Override this setting if BOLD is simulated!
+        self.saveAllActivity = (
+            False  # Save data from all chunks? Can be very memory demanding if simulations are long or large
+        )
+
+        # load default parameters if none were given
+        if params is None:
+            self.params = dp.loadDefaultParams(Cmat=self.Cmat, Dmat=self.Dmat, seed=self.seed)
+        else:
+            self.params = params
+
+        # set default output
+        self.setDefaultOutput(self.defaultOutput)
+
+    def run(self):
+        """
+        Runs the aLN mean-field model simulation
+        """
+        if self.simulateChunkwise:
+            t, x, y, t_BOLD, BOLD = cw.chunkwiseTimeIntegration(
+                self.params,
+                chunkSize=self.chunkSize,
+                simulateBOLD=self.simulateBOLD,
+                saveAllActivity=self.saveAllActivity,
+            )
+            self.setOutput("BOLD.t", t_BOLD)
+            self.setOutput("BOLD.BOLD", BOLD)
+
+        else:
+            t, x, y = ti.timeIntegration(self.params)
+
+        self.setOutput("t", t)
+        self.setOutput("x", x)
+        self.setOutput("y", y)
