37d73e4bbeb7dcee0ec8aeb13ef73d01d1939eb2
caglarcakan <sevensirk@gmail.com>
2020-02-23 17:29:57 +0100

    readme: pypi readme == github readme

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 from os import path
 
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, "README_pypi.md"), encoding="utf-8") as f:
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 with open("requirements.txt") as f:
