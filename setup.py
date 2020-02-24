e3b95c9a4cd336f6efed47413e8e23b094fa7744
caglarcakan <sevensirk@gmail.com>
2020-02-24 17:34:29 +0100

    readme: update, setup.py: OS Linux

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: Linux, macOS",
+        "Operating System :: Linux",
     ],
     python_requires=">=3.6",
     install_requires=requirements,
