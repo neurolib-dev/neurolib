9e22749379f197a25d926fe7e69d320831bef46e
caglarcakan <sevensirk@gmail.com>
2020-03-09 18:39:37 +0100

    models: do not normalize Cmat on loadDefaultParams

--- neurolib/models/aln/loadDefaultParams.py
+++ neurolib/models/aln/loadDefaultParams.py
@@ -55,7 +55,6 @@ def loadDefaultParams(Cmat=None, Dmat=None, lookupTableFileName=None, seed=None)
     else:
         params.Cmat = Cmat.copy()  # coupling matrix
         np.fill_diagonal(Cmat, 0)  # no self connections
-        params.Cmat = Cmat / np.max(Cmat)  # normalize matrix
         params.N = len(params.Cmat)  # number of nodes
         params.lengthMat = Dmat  # delay matrix
 
