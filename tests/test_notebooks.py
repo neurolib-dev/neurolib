e36043dd9ceb330919d05d8bd9156eab3dc5ac9e
caglarcakan <sevensirk@gmail.com>
2020-03-23 15:16:27 +0100

    notebook testing

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -1,44 +1,6 @@
 import os
 import unittest
 
-import nbformat
-from nbconvert.preprocessors import ExecutePreprocessor
-
-
-def run_notebook(notebook_path):
-    # From https://www.blog.pythonlibrary.org/2018/10/16/testing-jupyter-notebooks/
-    nb_name, _ = os.path.splitext(os.path.basename(notebook_path))
-    dirname = os.path.dirname(notebook_path)
-
-    with open(notebook_path) as f:
-        nb = nbformat.read(f, as_version=4)
-
-    proc = ExecutePreprocessor(timeout=600, kernel_name="python3")
-    proc.allow_errors = True
-
-    proc.preprocess(nb, {})
-    output_path = os.path.join(dirname, "{}_all_output.ipynb".format(nb_name))
-
-    with open(output_path, mode="wt") as f:
-        nbformat.write(nb, f)
-    errors = []
-    for cell in nb.cells:
-        if "outputs" in cell:
-            for output in cell["outputs"]:
-                if output.output_type == "error":
-                    errors.append(output)
-    os.remove(output_path)
-    return nb, errors
-
-
-def print_errors(fname, errors):
-    if len(errors) > 0:
-        print(f"Error in {fname}")
-    for error in errors:
-        if "evalue" in error:
-            print(error["ename"], error["evalue"])
-
-
 # from https://github.com/ReviewNB/treon/blob/master/tests/test_execution.py
 from treon.test_execution import execute_notebook
 
