345c9381e69dd9d78b7c18879e9dc58056bdc1ff
caglarcakan <sevensirk@gmail.com>
2020-04-08 14:41:01 +0200

    testing: explorationUtils.py

---
+++ tests/test_explorationUtils.py
@@ -0,0 +1,65 @@
+import unittest
+import numpy as np
+import pytest
+import sys
+
+from neurolib.models.fhn import FHNModel
+from neurolib.optimize.exploration import BoxSearch
+from neurolib.utils.parameterSpace import ParameterSpace
+from neurolib.utils.loadData import Dataset
+
+import neurolib.optimize.exploration.explorationUtils as eu
+
+
+class TestExplorationUtils(unittest.TestCase):
+    """
+    Test functions in neurolib/optimize/exploration/explorationUtils.py
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        ds = Dataset("hcp")
+        model = FHNModel(Cmat=ds.Cmat, Dmat=ds.Dmat)
+        model.params.duration = 10 * 1000  # ms
+        model.params.dt = 0.05
+        model.params.bold = True
+        parameters = ParameterSpace(
+            {
+                "x_ext": [np.ones((model.params["N"],)) * a for a in np.linspace(0, 2, 2)],
+                "K_gl": np.linspace(0, 2, 2),
+                "coupling": ["additive", "diffusive"],
+            },
+            kind="grid",
+        )
+        search = BoxSearch(model=model, parameterSpace=parameters, filename="TestExplorationUtils.hdf")
+
+        search.run(chunkwise=True, bold=True)
+
+        search.loadResults()
+        # flatten x_ext parameter
+        search.dfResults.x_ext = [a[0] for a in list(search.dfResults.x_ext)]
+
+        cls.model = model
+        cls.search = search
+        cls.ds = ds
+
+    def test_processExplorationResults(self):
+        self.search.dfResults = eu.processExplorationResults(
+            self.search.results, self.search.dfResults, model=self.model, ds=self.ds, bold_transient=0
+        )
+
+    def test_findCloseResults(self):
+        eu.findCloseResults(self.search.dfResults, dist=1, x_ext=0, K_gl=0.0)
+
+    @pytest.mark.skipif(sys.platform == "darwin", reason="plotting does not work on macOS")
+    def test_plotExplorationResults(self):
+        eu.plotExplorationResults(
+            self.search.dfResults,
+            par1=["x_ext", "$x_{ext}$"],
+            par2=["K_gl", "$K$"],
+            plot_key="max_x",
+            by=["coupling"],
+            by_label=["coupling"],
+            plot_key_label="testlabel",
+            one_figure=True,
+        )
