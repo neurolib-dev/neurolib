71d329e762ae7dc0b5948f20dcb27fd028692d78
caglorithm <sevensirk@gmail.com>
2020-04-15 20:20:49 +0200

    better utils

--- neurolib/optimize/exploration/explorationUtils.py
+++ neurolib/optimize/exploration/explorationUtils.py
@@ -5,6 +5,8 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 import logging
 import tqdm
 
+from scipy import stats
+
 from ...utils import functions as func
 
 
@@ -18,6 +20,7 @@ def plotExplorationResults(
     plot_key_label=None,
     symmetric_colorbar=False,
     one_figure=False,
+    **kwargs
 ):
     """
     """
@@ -36,40 +39,34 @@ def plotExplorationResults(
         par2_label = par2[1]
         par2 = par2[0]
 
+    # the `by` argument is used to slice the data
     # if no by value was given, create a dummy value
     if by is None:
         dfResults["_by"] = 0
         by = ["_by"]
 
+    if by_label is None:
+        by_label = by        
+
     n_plots = len(dfResults.groupby(by=by))
 
-    if by_label is None:
-        by_label = by
 
-    # if by is not None:
-    #     n_plots = len(dfResults.groupby(by=by))
-    # else:
-    #     n_plots = 1
-
+    # create subfigures
     if one_figure == True:
         fig, axs = plt.subplots(nrows=1, ncols=n_plots, figsize=(n_plots * 4, 3.5), dpi=150)
         if plot_key_label:
             fig.suptitle(plot_key_label)
 
-    # if by is not None:
-    #     _dfResults = dfResults.groupby(by=by)
-    # else:
-    #     _dfResults = dfResults
-
     axi = 0
+    # cycle through all slices
     for i, df in dfResults.groupby(by=by):
-
+        # chose the current axis
         if one_figure == True:
             if n_plots > 1:
                 ax = axs[axi]
             else:
                 ax = axs
-
+        # pivot data
         df_pivot = df.pivot_table(values=plot_key, index=par2, columns=par1, dropna=False)
 
         plot_clim = None
@@ -82,29 +79,34 @@ def plotExplorationResults(
                 plt.title(plot_key_label)
             ax = plt.gca()
 
+        image_extent = [min(df[par1]), max(df[par1]), min(df[par2]), max(df[par2])]
         im = ax.imshow(
             df_pivot,
-            extent=[min(df[par1]), max(df[par1]), min(df[par2]), max(df[par2])],
+            extent=image_extent,
             origin="lower",
             aspect="auto",
             clim=plot_clim,
         )
 
+        # plot contours
+        if "contour" in kwargs:
+            # check if this is a dataframe
+            if isinstance(kwargs["contour"], pd.DataFrame):
+                contour_color = kwargs["contour_color"] if "contour_color" in kwargs else "white"
+                contourPlotDf(kwargs["contour"], color=contour_color, ax=ax)
+
+
+        # colorbar
         if one_figure == False:
+            # useless and wrong if images don't have the same range! should be used only if plot_clim is used
             cbar = plt.colorbar(im, ax=ax, orientation="vertical", label=plot_key_label)
         else:
-            # # below is code for one colorbar only but it only displays the values of
-            # # the last plot so it's useless
-            # # if this is the last plot
-            # if axi == n_plots - 1:
-            #     cbar_ax = fig.add_axes([0.91, 0.18, 0.005, 0.65])
-            #     cbar = fig.colorbar(im, cax=cbar_ax, label=plot_key_label)
-
             # colorbar per plot
             divider = make_axes_locatable(ax)
             cax = divider.append_axes("right", size="5%", pad=0.05)
             fig.colorbar(im, cax=cax, orientation="vertical")
 
+        # labels and style
         ax.set_xlabel(par1_label)
         ax.set_ylabel(par2_label)
 
@@ -124,12 +126,46 @@ def plotExplorationResults(
         plt.show()
 
 
-def contourPlotDf(dataframe, color="white"):
-    Xi, Yi = np.meshgrid(range(len(dataframe.columns)), range(len(dataframe.index)))
-    cset2 = plt.contour(
+def contourPlotDf(dataframe, color="white", ax=None):
+    #Xi, Yi = np.meshgrid(range(len(dataframe.columns)), range(len(dataframe.index)))
+    Xi, Yi = np.meshgrid(dataframe.columns, dataframe.index)
+    ax = ax or plt
+    cset2 = ax.contour(
         Xi, Yi, dataframe, colors=color, linestyles="solid", levels=[0, 1.0001], linewidths=(4,), zorder=1
     )
 
+def plotResult(search, runId, **kwargs):
+    fig, axs = plt.subplots(1, 3, figsize=(10, 2), dpi=150, gridspec_kw={'width_ratios': [1, 2, 2]})
+
+    if "bold_transient" in kwargs:
+        bold_transient = int(kwargs["bold_transient"] / 2)
+    else:
+        bold_transient = int(30 / 2)
+
+    bold = search.results[runId].BOLD[:, bold_transient:]
+    bold_z = stats.zscore(bold, axis=1)
+    t_bold = np.linspace(2, len(bold.T)*2, len(bold.T), )
+
+    output = search.results[runId].output[:, :]
+    output_dt = search.model.params.dt
+    t_output = np.linspace(output_dt, len(output.T)*output_dt, len(output.T), )
+
+    axs[0].set_title(f"FC (run {runId})")
+    axs[0].imshow(func.fc(bold))
+    axs[0].set_ylabel("Node")
+    axs[0].set_xlabel("Node")
+    
+    #axs[1].set_title("BOLD")
+    axs[1].plot(t_bold, bold_z.T, lw=1.5, alpha=0.8);
+    axs[1].set_xlabel("Time [s]")
+    axs[1].set_ylabel("Normalized BOLD")
+
+    axs[2].set_ylabel("Activity")
+    axs[2].plot(t_output, output.T, lw=1.5, alpha=0.6);
+    axs[2].set_xlabel("Time [ms]")
+    if "xlim" in kwargs:
+        axs[2].set_xlim(kwargs["xlim"])
+    plt.tight_layout()
 
 def processExplorationResults(results, dfResults, **kwargs):
     """Process results from the exploration. 
@@ -147,7 +183,8 @@ def processExplorationResults(results, dfResults, **kwargs):
     # cycle through all results
     for i in tqdm.tqdm(dfResults.index):
         # if the result has an output
-        if "t" in results[i].keys() or "outputs.t" in results[i].keys():
+        #if "t" in results[i].keys() or "outputs.t" in results[i].keys():
+        if "BOLD" in results[i].keys():
             # if a dataset was passed as an argument
             if "ds" in kwargs:
                 ds = kwargs["ds"]
@@ -157,11 +194,19 @@ def processExplorationResults(results, dfResults, **kwargs):
                 dfResults.loc[i, "fc"] = np.mean(
                     [
                         func.matrix_correlation(
-                            func.fc(results[i]["BOLD"][:, results[i]["t_BOLD"] > bold_transient]), fc,
+                            func.fc(results[i]["BOLD"][:, int(bold_transient/1000/2):]), fc,
                         )
                         for fc in ds.FCs
                     ]
+                )         
+
+                dfResults.loc[i, "fcd"] = np.mean(
+                    [
+                        func.ts_kolmogorov(results[i]["BOLD"][:, int(bold_transient/1000/2):], bold)
+                        for bold in ds.BOLDs
+                    ]
                 )
+                            
 
             # if the model is passed as an argument
             if "model" in kwargs:
@@ -193,17 +238,19 @@ def processExplorationResults(results, dfResults, **kwargs):
     return dfResults
 
 
-def findCloseResults(dfResults, dist=0.01, **kwargs):
+def findCloseResults(dfResults, dist=0.01, relative = False, **kwargs):
     """Usage: findCloseResults(search.dfResults, mue_ext_mean=2.0, mui_ext_mean=2.5)
     """
     # dist = 0.01
     selectors = True
     for key, value in kwargs.items():
-        new_selector = abs(dfResults[key] - value) < dist
+        if relative:
+            new_selector = abs(dfResults[key] - value) <= dist * value
+        else:
+            new_selector = abs(dfResults[key] - value) <= dist
         selectors = selectors & new_selector
     filtered_df = dfResults[selectors]
     return filtered_df
-
 
 def paramsRun(dfResults, runNr):
     return dfResults.loc[runNr].to_dict()
