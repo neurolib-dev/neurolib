2ad182ce4f70df4c2e431f181849df35934e48af
caglarcakan <sevensirk@gmail.com>
2020-04-28 20:51:07 +0200

    pypet logging config

--- neurolib/utils/paths.py
+++ neurolib/utils/paths.py
@@ -4,3 +4,5 @@ This file is a configuration file that contains default directories for saving d
 
 HDF_DIR = "./data/hdf/"
 FIGURES_DIR = "./data/figures/"
+
+PYPET_LOGGING_CONFIG = "./neurolib/utils/pypet_logging.ini"
