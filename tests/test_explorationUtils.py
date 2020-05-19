f506de230cff85bc7e2925f704cd1dc28997a50b
caglarcakan <caglorithm@gmail.com>
2020-05-19 23:13:00 +0200

    testing: ALN for plotting

--- tests/test_explorationUtils.py
+++ tests/test_explorationUtils.py
@@ -3,7 +3,7 @@ import numpy as np
 import pytest
 import sys
 
-from neurolib.models.fhn import FHNModel
+from neurolib.models.aln import ALNModel
 from neurolib.optimize.exploration import BoxSearch
 from neurolib.utils.parameterSpace import ParameterSpace
 from neurolib.utils.loadData import Dataset
@@ -28,15 +28,14 @@ class TestExplorationUtils(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         ds = Dataset("hcp")
-        model = FHNModel(Cmat=ds.Cmat, Dmat=ds.Dmat)
-        model.params.duration = 10 * 1000  # ms
-        model.params.dt = 0.1
-        model.params.bold = True
+        model = ALNModel(Cmat=ds.Cmat, Dmat=ds.Dmat)
+        model.params.duration = 11 * 1000  # ms
+        model.params.dt = 0.2
         parameters = ParameterSpace(
             {
-                "x_ext": [np.ones((model.params["N"],)) * a for a in np.linspace(0, 2, 2)],
-                "K_gl": np.linspace(0, 2, 2),
-                "coupling": ["additive", "diffusive"],
+                "mue_ext_mean": np.linspace(0, 3, 2), 
+                "mui_ext_mean": np.linspace(0, 3, 2)
+                "b": [0.0, 10.0],
             },
             kind="grid",
         )
@@ -68,25 +67,29 @@ class TestExplorationUtils(unittest.TestCase):
             self.search, model=self.model, ds=self.ds, bold_transient=0
         )
 
+        # one_figure = True
+        # alpha_mask
+        # contour
         eu.plotExplorationResults(
             self.search.dfResults,
-            par1=["x_ext", "$x_{ext}$"],
-            par2=["K_gl", "$K$"],
+            par1=["mue_ext_mean", "$mue$"],
+            par2=["mui_ext_mean", "$mui$"],
             plot_key="max_" + self.model.default_output,
             contour="max_" + self.model.default_output,
             alpha_mask="max_" + self.model.default_output,
-            by=["coupling"],
-            by_label=["coupling"],
+            by=["b"],
+            by_label=["b"],
             plot_key_label="testlabel",
             one_figure=True,
         )
 
+        # one_figure = False
         eu.plotExplorationResults(
             self.search.dfResults,
-            par1=["x_ext", "$x_{ext}$"],
-            par2=["K_gl", "$K$"],
+            par1=["mue_ext_mean", "$mue$"],
+            par2=["mui_ext_mean", "$mui$"],
             plot_key="max_" + self.model.default_output,
-            by_label=["coupling"],
+            by_label=["b"],
             plot_key_label="testlabel",
             one_figure=False,
         )
