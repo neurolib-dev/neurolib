af18ee202e703efdea105f289292bf3ac3b0c752
caglorithm <sevensirk@gmail.com>
2020-06-04 12:02:39 +0200

    plotExplorationResults: alpha maps

--- neurolib/optimize/exploration/explorationUtils.py
+++ neurolib/optimize/exploration/explorationUtils.py
@@ -18,6 +18,7 @@ def plotExplorationResults(
     par1,
     par2,
     plot_key,
+    nan_to_zero=False,
     by=None,
     by_label=None,
     plot_key_label=None,
@@ -86,7 +87,10 @@ def plotExplorationResults(
         # pivot data and plot
         df_pivot = df.pivot_table(values=plot_key, index=par2, columns=par1, dropna=False)
 
-        plot_clim = (np.nanmin(df_pivot.values), np.nanmax(df_pivot.values))
+        if nan_to_zero:
+            df_pivot = df_pivot.fillna(0)
+
+        plot_clim = kwargs["plot_clim"] if "plot_clim" in kwargs else (np.nanmin(df_pivot.values), np.nanmax(df_pivot.values))
 
         if symmetric_colorbar:
             plot_clim = (-np.max(np.abs(plot_clim)), np.max(np.abs(plot_clim)))
@@ -112,6 +116,8 @@ def plotExplorationResults(
                 mask = df.pivot_table(values=alpha_mask, index=par2, columns=par1, dropna=False)
             elif isinstance(alpha_mask, str):
                 mask = df.pivot_table(values=alpha_mask, index=par2, columns=par1, dropna=False)
+                if nan_to_zero:
+                    mask = mask.fillna(0)                
                 mask = np.array(mask)
             else:
                 mask = None
@@ -132,13 +138,17 @@ def plotExplorationResults(
         if contour is not None:
             contour_color = kwargs["contour_color"] if "contour_color" in kwargs else "white"
             contour_levels = kwargs["contour_levels"] if "contour_levels" in kwargs else None
+            contour_alpha = kwargs["contour_alpha"] if "contour_alpha" in kwargs else 1
+
             # check if this is a dataframe
             if isinstance(contour, pd.DataFrame):
-                contourPlotDf(contour, color=contour_color, ax=ax, levels=contour_levels)
+                contourPlotDf(contour, color=contour_color, ax=ax, levels=contour_levels, alpha=contour_alpha)
             # if it's a string, take that value as the contour plot value
             elif isinstance(contour, str):
                 df_contour = df.pivot_table(values=contour, index=par2, columns=par1, dropna=False)
-                contourPlotDf(df_contour, color=contour_color, ax=ax, levels=contour_levels)            
+                if nan_to_zero:
+                    df_contour = df_contour.fillna(0)
+                contourPlotDf(df_contour, color=contour_color, ax=ax, levels=contour_levels, alpha=contour_alpha)            
 
         # colorbar
         if one_figure == False:
@@ -169,12 +179,12 @@ def plotExplorationResults(
         plt.show()
 
 
-def contourPlotDf(dataframe, color="white", levels=None, ax=None):
+def contourPlotDf(dataframe, color="white", levels=None, ax=None, alpha=1.0):
     levels = levels or [0, 1.0001]
     Xi, Yi = np.meshgrid(dataframe.columns, dataframe.index)
     ax = ax or plt
     cset2 = ax.contour(
-        Xi, Yi, dataframe, colors=color, linestyles="solid", levels=levels, linewidths=(4,), zorder=1
+        Xi, Yi, dataframe, colors=color, linestyles="solid", levels=levels, linewidths=(4,), zorder=1, alpha=alpha
     )
 
 def alphaMask(image, threshold, alpha, mask=None, invert=False, style=None):
