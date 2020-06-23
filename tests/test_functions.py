f943392cb09bb3a785ed221cffbd4e4c0be4673d
caglarcakan <caglorithm@gmail.com>
2020-06-23 12:18:21 +0200

    test: func.weighted_correlation

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -31,7 +31,9 @@ class TestFunctions(unittest.TestCase):
         cls.single_node = ALNModel()
 
     def test_kuramoto(self):
-        kuramoto = func.kuramoto(self.model.rates_exc[:, ::10], dt=self.model.params["dt"], smoothing=1.0)
+        kuramoto = func.kuramoto(
+            self.model.rates_exc[:, ::10], dt=self.model.params["dt"], smoothing=1.0
+        )
 
     def test_fc(self):
         FC = func.fc(self.model.BOLD.BOLD)
@@ -43,22 +45,58 @@ class TestFunctions(unittest.TestCase):
         FC = func.fc(self.model.BOLD.BOLD)
         cc = func.matrix_correlation(FC, self.ds.FCs[0])
 
+    def test_weighted_correlation(self):
+        x = self.model.rates_exc[0, :]
+        y = self.model.rates_exc[1, :]
+        w = np.ones(xshape)
+        cc = func.weighted_correlation(x, y, w)
+
     def test_ts_kolmogorov(self):
-        func.ts_kolmogorov(self.model.rates_exc[::20, :], self.model.rates_exc, stepsize=250, windowsize=30)
+        func.ts_kolmogorov(
+            self.model.rates_exc[::20, :],
+            self.model.rates_exc,
+            stepsize=250,
+            windowsize=30,
+        )
 
     def test_matrix_kolmogorov(self):
-        func.matrix_kolmogorov(func.fc(self.model.rates_exc[::20, :]), func.fc(self.model.rates_exc[::20, :]))
+        func.matrix_kolmogorov(
+            func.fc(self.model.rates_exc[::20, :]),
+            func.fc(self.model.rates_exc[::20, :]),
+        )
 
     def test_getPowerSpectrum(self):
-        fr, pw = func.getPowerSpectrum(self.model.rates_exc[0, :], dt=self.model.params["dt"])
+        fr, pw = func.getPowerSpectrum(
+            self.model.rates_exc[0, :], dt=self.model.params["dt"]
+        )
 
     def test_getMeanPowerSpectrum(self):
-        fr, pw = func.getMeanPowerSpectrum(self.model.rates_exc, dt=self.model.params["dt"])
+        fr, pw = func.getMeanPowerSpectrum(
+            self.model.rates_exc, dt=self.model.params["dt"]
+        )
 
     def test_construct_stimulus(self):
-        self.single_node.params['duration'] = 2000
-        stimulus = func.construct_stimulus("ac", duration=self.single_node.params.duration, dt=self.single_node.params.dt, stim_amp=1.0, stim_freq=1)
-        stimulus = func.construct_stimulus("dc", duration=self.single_node.params.duration, dt=self.single_node.params.dt, stim_amp=1.0, stim_freq=1)
-        stimulus = func.construct_stimulus("rect", duration=self.single_node.params.duration, dt=self.single_node.params.dt, stim_amp=1.0, stim_freq=1)
-        
+        self.single_node.params["duration"] = 2000
+        stimulus = func.construct_stimulus(
+            "ac",
+            duration=self.single_node.params.duration,
+            dt=self.single_node.params.dt,
+            stim_amp=1.0,
+            stim_freq=1,
+        )
+        stimulus = func.construct_stimulus(
+            "dc",
+            duration=self.single_node.params.duration,
+            dt=self.single_node.params.dt,
+            stim_amp=1.0,
+            stim_freq=1,
+        )
+        stimulus = func.construct_stimulus(
+            "rect",
+            duration=self.single_node.params.duration,
+            dt=self.single_node.params.dt,
+            stim_amp=1.0,
+            stim_freq=1,
+        )
+
         self.single_node.run()
