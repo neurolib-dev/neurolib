48648bce27a0120e43c38583bd927d0a0f43bf49
Nikola Jajcay <nikola.jajcay@gmail.com>
2020-06-25 12:37:51 +0200

    another little renaming

--- neurolib/models/multimodel/builder/base/constants.py
+++ neurolib/models/multimodel/builder/base/constants.py
@@ -11,8 +11,8 @@ NETWORK_NAME_STR = "net"
 NODE_NAME_STR = "node"
 MASS_NAME_STR = "mass"
 # names for matrices
-NODAL_CONNECTIVITY = "local_connectivity"
-NODAL_DELAYS = "local_delays"
+NODE_CONNECTIVITY = "local_connectivity"
+NODE_DELAYS = "local_delays"
 NETWORK_CONNECTIVITY = "connectivity"
 NETWORK_DELAYS = "delays"
 
