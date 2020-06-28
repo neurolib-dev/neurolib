b4eca7641f6e4e93f58b3e2c51392b5abe66fea2
Nikola Jajcay <nikola.jajcay@gmail.com>
2020-06-28 16:32:34 +0200

    support seed for initial conditions

--- neurolib/models/multimodel/builder/hopf.py
+++ neurolib/models/multimodel/builder/hopf.py
@@ -41,14 +41,15 @@ class HopfMass(NeuralMass):
     required_params = ["a", "omega", "ext_input_x", "ext_input_y"]
     required_couplings = ["network_x", "network_y"]
 
-    def __init__(self, params=None):
-        super().__init__(params=params or DEFAULT_PARAMS)
+    def __init__(self, params=None, seed=None):
+        super().__init__(params=params or DEFAULT_PARAMS, seed=seed)
 
     def _initialize_state_vector(self):
         """
         Initialize state vector.
         """
-        self.initial_state = [0.5 * np.random.uniform(-1, 1)] * self.num_state_variables
+        np.random.seed(self.seed)
+        self.initial_state = (0.5 * np.random.uniform(-1, 1, size=(self.num_state_variables,))).tolist()
 
     def _derivatives(self, coupling_variables):
         [x, y] = self._unwrap_state_vector()
@@ -84,12 +85,14 @@ class HopfNetworkNode(Node):
     default_network_coupling = {"network_x": 0.0, "network_y": 0.0}
     default_output = "x"
 
-    def __init__(self, params=None):
+    def __init__(self, params=None, seed=None):
         """
         :param params: parameters of the Hopf mass
         :type params: dict|None
+        :param seed: seed for random number generator
+        :type seed: int|None
         """
-        hopf_mass = HopfMass(params)
+        hopf_mass = HopfMass(params, seed=seed)
         hopf_mass.index = 0
         super().__init__(neural_masses=[hopf_mass])
 
@@ -108,7 +111,7 @@ class HopfNetwork(Network):
     sync_variables = ["network_x", "network_y"]
 
     def __init__(
-        self, connectivity_matrix, delay_matrix, mass_params=None, x_coupling="diffusive", y_coupling="none",
+        self, connectivity_matrix, delay_matrix, mass_params=None, x_coupling="diffusive", y_coupling="none", seed=None,
     ):
         """
         :param connectivity_matrix: connectivity matrix for between nodes
@@ -128,12 +131,15 @@ class HopfNetwork(Network):
         :param y_coupling: how to couple `y` variables in the nodes,
             "diffusive", "additive", or "none"
         :type y_coupling: str
+        :param seed: seed for random number generator
+        :type seed: int|None
         """
         mass_params = self._prepare_mass_params(mass_params, connectivity_matrix.shape[0])
+        seeds = self._prepare_mass_params(seed, connectivity_matrix.shape[0], native_type=int)
 
         nodes = []
         for i, node_params in enumerate(mass_params):
-            node = HopfNetworkNode(params=node_params)
+            node = HopfNetworkNode(params=node_params, seed=seeds[i])
             node.index = i
             node.idx_state_var = i * node.num_state_variables
             nodes.append(node)
