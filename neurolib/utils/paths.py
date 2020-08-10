3b07beab20490c2c6c20b41e4398d67b7256d72b
caglarcakan <caglorithm@gmail.com>
2020-08-10 17:15:56 +0200

    fix path

--- neurolib/utils/paths.py
+++ neurolib/utils/paths.py
@@ -1,8 +1,9 @@
 """
 This file is a configuration file that contains default directories for saving data
 """
+import os
 
 HDF_DIR = "./data/hdf/"
 FIGURES_DIR = "./data/figures/"
 
-PYPET_LOGGING_CONFIG = "./neurolib/utils/pypet_logging.ini"
+PYPET_LOGGING_CONFIG = os.path.join(os.path.dirname(__file__), "pypet_logging.ini")
