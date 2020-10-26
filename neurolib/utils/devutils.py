5eaa7600f9423a945d99ba44f75373997fe30712
caglarcakan <caglorithm@gmail.com>
2020-10-26 08:44:50 +0100

    devUtils: added get_size() function for debugging

--- neurolib/utils/devutils.py
+++ neurolib/utils/devutils.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy
 
+import sys
+
 import neurolib.utils.functions as func
 
 
@@ -333,3 +335,24 @@ def rolling_window(array, window=(0,), asteps=None, wsteps=None, axes=None, toen
     new_shape = new_shape[new_shape != 0]
 
     return np.lib.stride_tricks.as_strided(array, shape=new_shape, strides=new_strides)
+
+
+def get_size(obj, seen=None):
+    """Recursively finds size of objects"""
+    size = sys.getsizeof(obj)
+    if seen is None:
+        seen = set()
+    obj_id = id(obj)
+    if obj_id in seen:
+        return 0
+    # Important mark as seen *before* entering recursion to gracefully handle
+    # self-referential objects
+    seen.add(obj_id)
+    if isinstance(obj, dict):
+        size += sum([get_size(v, seen) for v in obj.values()])
+        size += sum([get_size(k, seen) for k in obj.keys()])
+    elif hasattr(obj, "__dict__"):
+        size += get_size(obj.__dict__, seen)
+    elif hasattr(obj, "__iter__") and not isinstance(obj, (str, bytes, bytearray)):
+        size += sum([get_size(i, seen) for i in obj])
+    return size
