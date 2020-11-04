a28d3eee91d26b326d54c3338974aaf5cc33ec30
Nikola Jajcay <nikola.jajcay@gmail.com>
2020-11-04 19:08:39 +0100

    continue multimodel - integrate and simulate BOLD done

--- neurolib/models/multimodel/model.py
+++ neurolib/models/multimodel/model.py
@@ -1,6 +1,8 @@
 import logging
 
 import numpy as np
+from chspy import join
+from numpy.core.fromnumeric import var
 
 from ...utils.collections import dotdict, flatten_nested_dict, star_dotdict
 from ..model import Model
@@ -70,22 +72,98 @@ class MultiModel(Model):
             params.update({"N": 1, "Cmat": np.zeros((1, 1))})
         else:
             params.update({"N": len(self.model_instance.nodes), "Cmat": self.model_instance.connectivity})
+        # TODO add noise params here
         return params
 
-    def initializeRun(self, initializeBold=False):
-        pass
-
     def getMaxDelay(self):
-        pass
+        """
+        Return max delay in units of dt. In ms, this is given as a property in the model instance.
+        """
+        return int(np.around(self.model_instance.max_delay / self.params["dt"]))
 
     def run(self):
         pass
 
-    def integrate(self, append_outputs, simulate_bold):
+    def _init_noise_inputs(self, backend):
+        """
+        Build noise / stimulus input to the model.
+        """
+        if backend == "jitcdde":
+            init_func = lambda noise: noise.as_cubic_splines(duration=self.params["duration"], dt=self.params["dt"])
+            join_func = lambda x: join(*x)
+        elif backend == "numba":
+            init_func = lambda noise: noise.as_array(duration=self.params["duration"], dt=self.params["dt"])
+            join_func = lambda x: np.hstack(x)
+        else:
+            raise ValueError(f"Unknown backend {backend}")
+        # initialise each noise / stimulation process and join
+        return join_func([init_func(noise) for noise in self.model_instance.noise_input])
+
+    def integrate(self, append_outputs=False, simulate_bold=False, noise_input=None):
+        """
+        :param noise_input: custom noise input if desired, if None, will use
+            default, it's type depends on backend:
+            - for `numba` backend as np.ndarray
+            -for `jitcdde` backend as interpolated Cubic Hermite Splines
+                (`chspy.CubicHermiteSpline`)
+        :type noise_input: np.ndarray|chspy.CubicHermiteSpline
+        """
+        noise_input = noise_input or self._init_noise_inputs(self.params["backend"])
         result = self.model_instance.run(
             duration=self.params["duration"],
             dt=self.params["dt"],
-            noise_input=None,
+            noise_input=noise_input,
             backend=self.params["backend"],
             return_xarray=True,
         )
+        self.storeOutputsAndStates(result, append=append_outputs)
+        # force bold if params['bold'] == True
+        if "bold" in self.params:
+            if self.params["bold"]:
+                simulate_bold = True
+
+        # bold simulation after integration
+        if simulate_bold and self.boldInitialized:
+            self.simulateBold(result[self.default_output].values, append=True)
+
+    def storeOutputsAndStates(self, results, append):
+        # save time array
+        self.setOutput("t", results.time.values(), append=append, removeICs=True)
+        self.setStateVariables("t", results.time.values())
+        # save outputs
+        for variable in results:
+            if variable in self.output_vars:
+                self.setOutput(variable, results[variable].values, append=append, removeICs=True)
+            self.setStateVariables(variable, results[variable].values)
+
+    def simulateBold(self, bold_variable, append):
+        if self.boldInitialized:
+            bold_input = bold_variable[:, self.startindt :]
+            if bold_input.shape[1] >= self.boldModel.samplingRate_NDt:
+                # only if the length of the output has a zero mod to the sampling rate,
+                # the downsampled output from the boldModel can correctly appended to previous data
+                # so: we are lazy here and simply disable appending in that case ...
+                if not bold_input.shape[1] % self.boldModel.samplingRate_NDt == 0:
+                    append = False
+                    logging.warn(
+                        f"Output size {bold_input.shape[1]} is not a multiple of BOLD sample length { self.boldModel.samplingRate_NDt}, will not append data."
+                    )
+                logging.debug(f"Simulating BOLD: boldModel.run(append={append})")
+
+                # transform bold input according to self.boldInputTransform
+                if self.boldInputTransform:
+                    bold_input = self.boldInputTransform(bold_input)
+
+                # simulate bold model
+                self.boldModel.run(bold_input, append=append)
+
+                t_BOLD = self.boldModel.t_BOLD
+                BOLD = self.boldModel.BOLD
+                self.setOutput("BOLD.t_BOLD", t_BOLD)
+                self.setOutput("BOLD.BOLD", BOLD)
+            else:
+                logging.warn(
+                    f"Will not simulate BOLD if output {bold_input.shape[1]*self.params['dt']} not at least of duration {self.boldModel.samplingRate_NDt*self.params['dt']}"
+                )
+        else:
+            logging.warn("BOLD model not initialized, not simulating BOLD. Use `run(bold=True)`")
