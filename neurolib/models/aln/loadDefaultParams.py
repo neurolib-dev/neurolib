ff7de24e1e5a2e9d9063a0f43327480d6304cd4a
caglarcakan <caglorithm@gmail.com>
2020-11-09 15:55:49 +0100

    remove sampling_dt from defaultParams

--- neurolib/models/aln/loadDefaultParams.py
+++ neurolib/models/aln/loadDefaultParams.py
@@ -30,7 +30,6 @@ def loadDefaultParams(Cmat=None, Dmat=None, lookupTableFileName=None, seed=None)
 
     # runtime parameters
     params.dt = 0.1  # ms 0.1ms is reasonable
-    params.sampling_dt = params.dt  # sampling dt (ms)
     params.duration = 2000  # Simulation duration (ms)
     np.random.seed(seed)  # seed for RNG of noise and ICs
     params.seed = seed
