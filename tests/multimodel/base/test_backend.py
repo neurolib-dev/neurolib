fc8940cd37e899e5cbacde1ce5c5e4df2d50c85a
Nikola Jajcay <nikola.jajcay@gmail.com>
2021-01-27 23:02:33 +0100

    start working on faster backend, but symengine vs sympy problems

--- tests/multimodel/base/test_backend.py
+++ tests/multimodel/base/test_backend.py
@@ -92,15 +92,6 @@ class TestNumbaBackend(unittest.TestCase):
         self.assertTrue(hasattr(backend, "_replace_inputs"))
         self.assertTrue(hasattr(backend, "_substitute_helpers"))
 
-    def test_float_parameters_to_symbolic(self):
-        backend = NumbaBackend()
-        DICT_IN = {"a": 40.0, "b": 15, "conn": np.random.rand(4, 4)}
-        result = backend.float_parameters_to_symbolic(DICT_IN)
-        for k, v in result.items():
-            self.assertTrue(isinstance(v, (sp.Symbol, sp.MatrixSymbol)))
-            if isinstance(v, sp.MatrixSymbol):
-                self.assertTupleEqual(v.shape, DICT_IN[k].shape)
-
     def test_replace_current_ys(self):
         backend = NumbaBackend()
         STRING_IN = "0.4*(1.0*(1.0 - current_y(0))"
