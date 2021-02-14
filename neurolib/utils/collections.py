f199df585d4d4af0d35f65d1f866b531acc365df
caglarcakan <caglorithm@gmail.com>
2021-02-14 16:03:23 +0100

    build and push to remore repo

--- neurolib/utils/collections.py
+++ neurolib/utils/collections.py
@@ -14,7 +14,16 @@ BACKWARD_REPLACE = {v: k for k, v in FORWARD_REPLACE.items()}
 
 
 class dotdict(dict):
-    """dot.notation access to dictionary attributes"""
+    """dot.notation access to dictionary attributes.
+
+    Example:
+
+    ```
+    model.params['duration'] = 10 * 1000 # classic key-value dictionary
+    model.params.duration = 10 * 10000 # easy access via dotdict
+    ```
+
+    """
 
     __getattr__ = dict.get
     __setattr__ = dict.__setitem__
@@ -32,6 +41,7 @@ class star_dotdict(dotdict):
     """
     Supports star notation in dotdict. Nested dicts are now treated as glob.
     Also supports minus as a pipe ("|") for filtering out strings after |.
+
     Example:
         Wilson-Cowan node has in total four parameters named tau (time constants
         for both excitatory and inhibitory populations, and Ornstein-Uhlenbeck
@@ -44,17 +54,17 @@ class star_dotdict(dotdict):
         'WCnode_0.WCmassINH_1.tau': 3.75,
         'WCnode_0.WCmassINH_1.noise_0.tau': 5.0}
 
-        Now imagine you want to make exploration over population time constants,
-        but keep O-U as is, you can:
+    Now let's imagine you want to make exploration over population time constants,
+    but keep O-U as is, you can:
 
         > model.params["*tau|noise"]
         # returns
         {'WCnode_0.WCmassEXC_0.tau': 2.5, 'WCnode_0.WCmassINH_1.tau': 3.75}
 
-        In other words, the string after "|" is filtered out from all the keys.
-        This works with setting, getting, and deleting an item and also
-        parameters defined with minus can be used in `Evolution` and
-        `Exploration` classes
+    In other words, the string after "|" is filtered out from all the keys.
+    This works with setting, getting, and deleting an item and also
+    parameters defined with minus can be used in `Evolution` and
+    `Exploration` classes.
     """
 
     def __getitem__(self, attr):
