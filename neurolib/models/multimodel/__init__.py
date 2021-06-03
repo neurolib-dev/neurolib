2c0cff06d2ad6a438d01bd3f852a4b0cbbd5ba0c
Nikola Jajcay <nikola.jajcay@gmail.com>
2021-06-03 15:51:14 +0200

    imports to multimodel init

--- neurolib/models/multimodel/__init__.py
+++ neurolib/models/multimodel/__init__.py
@@ -1,1 +1,7 @@
+from .builder.aln import ALNNetwork, ALNNode
+from .builder.fitzhugh_nagumo import FitzHughNagumoNetwork, FitzHughNagumoNode
+from .builder.hopf import HopfNetwork, HopfNode
+from .builder.thalamus import ThalamicNode
+from .builder.wilson_cowan import WilsonCowanNetwork, WilsonCowanNode
+from .builder.wong_wang import ReducedWongWangNetwork, ReducedWongWangNode, WongWangNetwork, WongWangNode
 from .model import MultiModel
