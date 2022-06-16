0856e75d081ad7ffa89f18efbbc6121db146213a
Lena Salfenmoser <lena.salfenmoser@tu-berlin.de>
2022-06-16 10:41:56 +0200

    branch PC development

--- neurolib/models/fhn/timeIntegration.py
+++ neurolib/models/fhn/timeIntegration.py
@@ -2,6 +2,7 @@ import numpy as np
 import numba
 
 from . import loadDefaultParams as dp
+from neurolib.utils import model_utils as mu
 
 
 def timeIntegration(params):
@@ -82,13 +83,14 @@ def timeIntegration(params):
     x_ou = params["x_ou"]
     y_ou = params["y_ou"]
 
-    x_ext = params["x_ext"]
-    y_ext = params["y_ext"]
-
     # state variable arrays, have length of t + startind
     # they store initial conditions AND simulated data
     xs = np.zeros((N, startind + len(t)))
     ys = np.zeros((N, startind + len(t)))
+
+    x_ext = mu.adjustArrayShape(params["x_ext"], xs)
+    y_ext = mu.adjustArrayShape(params["y_ext"], ys)
+
 
     # ------------------------------------------------------------------------
     # Set initial values
@@ -228,13 +230,13 @@ def timeIntegration_njit_elementwise(
                 - ys[no, i - 1]
                 + xs_input_d[no]  # input from other nodes
                 + x_ou[no]  # ou noise
-                + x_ext[no]  # external input
+                + x_ext[no, i]  # external input
             )
             y_rhs = (
                 (xs[no, i - 1] - delta - epsilon * ys[no, i - 1]) / tau
                 + ys_input_d[no]  # input from other nodes
                 + y_ou[no]  # ou noise
-                + y_ext[no]  # external input
+                + y_ext[no, i]  # external input
             )
 
             # Euler integration
