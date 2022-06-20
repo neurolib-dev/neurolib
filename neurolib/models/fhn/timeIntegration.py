92b761cb94c358c2fe2a655fafff9aeccc3e4ec0
Lena Salfenmoser <lena.salfenmoser@tu-berlin.de>
2022-06-20 16:43:40 +0200

    rebase

--- neurolib/models/fhn/timeIntegration.py
+++ neurolib/models/fhn/timeIntegration.py
@@ -137,8 +137,6 @@ def timeIntegration(params):
         ys,
         xs_input_d,
         ys_input_d,
-        x_ext_const,
-        y_ext_const,
         x_ext,
         y_ext,
         alpha,
@@ -176,8 +174,6 @@ def timeIntegration_njit_elementwise(
     ys,
     xs_input_d,
     ys_input_d,
-    x_ext_const,
-    y_ext_const,
     x_ext,
     y_ext,
     alpha,
@@ -251,46 +247,3 @@ def timeIntegration_njit_elementwise(
             y_ou[no] = y_ou[no] + (y_ou_mean - y_ou[no]) * dt / tau_ou + sigma_ou * sqrt_dt * noise_ys[no]  # mV/ms
 
     return t, xs, ys, x_ou, y_ou
-
-
-def adjust_shape(original, target):
-    """
-    Tiles and then cuts an array (or list or float) such that
-    it has the same shape as target at the end.
-    This is used to make sure that any input parameter like external current has
-    the same shape as the rate array.
-    """
-
-    # make an ext_exc_current ARRAY from a LIST or INT
-    if not hasattr(original, "__len__"):
-        original = [original]
-    original = np.array(original)
-
-    # repeat original in y until larger (or same size) as target
-
-    # tile until N
-
-    # either (x,) shape or (y,x) shape
-    if len(original.shape) == 1:
-        # if original.shape[0] > 1:
-        rep_y = target.shape[0]
-    elif target.shape[0] > original.shape[0]:
-        rep_y = int(target.shape[0] / original.shape[0]) + 1
-    else:
-        rep_y = 1
-
-    # tile once so the array has shape (N,1)
-    original = np.tile(original, (rep_y, 1))
-
-    # tile until t
-
-    if target.shape[1] > original.shape[1]:
-        rep_x = int(target.shape[1] / original.shape[1]) + 1
-    else:
-        rep_x = 1
-    original = np.tile(original, (1, rep_x))
-
-    # cut from end because the beginning can be initial condition
-    original = original[: target.shape[0], -target.shape[1] :]
-
-    return original
