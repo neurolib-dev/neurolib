89c5fac92a9df708db95046dfb740c0dd99f2b80
Martin <martinkrueck@gmx.de>
2022-07-04 12:53:43 +0200

    - removed old comments and empty spaces - restructured optimal_control-package to allow for model specific subpackages

--- neurolib/optimal_control/oc_fhn/oc_fhn.py
+++ neurolib/optimal_control/oc_fhn/oc_fhn.py
@@ -2,6 +2,7 @@ import numpy as np
 import numba
 from neurolib.optimal_control import cost_functions
 
+
 class OcFhn:
 
     def __init__(self, fhn_model, target, w_p=1, w_2=1, print_array=[]):
@@ -44,7 +45,7 @@ class OcFhn:
         # ToDo: maybe add input to both?
         self.control = np.vstack((self.model.params["x_ext"], self.model.params["y_ext"]))
 
-        self.cost_history = None #np.zeros(self.T)
+        self.cost_history = None    # np.zeros(self.T)
         self.cost_history_index = 0
 
         self.x_controls = self.model.params["x_ext"]    # save control signals throughout optimization iterations for
@@ -142,8 +143,6 @@ class OcFhn:
         # ToDo: generalize, not only precision cost
         fx = cost_functions.derivative_precision_cost(self.target, self.get_xs(), self.w_p)
 
-        #print("fx = ", fx)
-
         adjoint_state = np.zeros(self.output_dim)
         adjoint_state[:, -1] = 0
 
@@ -151,10 +150,7 @@ class OcFhn:
             adjoint_state[:, ind] = adjoint_state[:, ind + 1] \
                                     - (fx[:, ind + 1] + adjoint_state[:, ind + 1] @ hx[ind + 1]) * self.dt
 
-
         self.adjoint_state = adjoint_state
-
-        #print("adjoint = ", adjoint_state[0,:])
 
     def step_size(self, cost_gradient):
         """
@@ -186,7 +182,6 @@ class OcFhn:
                 break
 
         cost = self.compute_total_cost()
-        #print(f"cost0: %s, cost: %s" % (cost0, cost))
         while cost > cost0:
             step *= factor
             counter += 1
@@ -206,35 +201,21 @@ class OcFhn:
                 self.update_input()
                 break
 
-        # print(f"step: %s" % (step))
-
     def optimize(self, n_max_iterations):
         """ Compute the optimal control signal.
         """
         self.cost_history = np.zeros(n_max_iterations)
         self.cost_history_index = 0
         # (I) forward simulation
-        # self.model.params["x_ext"] = self.control
-        # self.model.params["y_ext"] =
         self.simulate_forward()  # yields x(t)
 
         # (II) control gradient happens within "step_size"
-        # c_grad = control_gradient(target, control0, x0)
-        # c_grad = self.compute_gradient()
-
         # (III) step size and control update
-        # step, cost, control1 = step_size(control0, target, c_grad, x0, duration, dt)
-        # step = self.step_size()
         grad = self.compute_gradient()
-
         self.step_size(-grad)
         self.x_grads = grad[0, :]
 
-        #print('gradient = ', grad[0,:])
-
         # (IV) forward simulation
-        # self.model.params["x_ext"] =  updated within "step_size"
-        # self.model.params["y_ext"] =  updated within "step_size"
         self.simulate_forward()
 
         for i in range(n_max_iterations):
@@ -242,17 +223,10 @@ class OcFhn:
                 print(f"Cost in iteration %s: %s" % (i, self.compute_total_cost()))
             self.add_cost_to_history(self.compute_total_cost())
             # (V.I) control gradient happens within "step_size"
-            # c_grad = control_gradient(target, control1, x0)
-
             # (V.II) step size and control update
-            # step, cost, control1 = step_size(control1, target, c_grad, x0, duration, dt)
-            # step = self.step_size()
             grad = self.compute_gradient()
             self.step_size(-grad)
             self.x_grads = np.vstack((self.x_grads, grad))
-            #print('gradient = ', grad[0,:])
 
             # (V.III) forward simulation
-            # self.model.params["x_ext"] =
-            # self.model.params["y_ext"] =
             self.simulate_forward()  # yields x(t)
