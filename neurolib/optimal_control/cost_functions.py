- fix: added scaling by time step of precision- and energy cost, added parameter in function calls - adjusted corresponding test cases

neurolib/optimal_control/cost_functions.py
@@ -3,7 +3,7 @@ import numba
 
 
 @numba.njit
-def precision_cost(x_target, x_sim, w_p, precision_matrix, interval=(0, None)):
+def precision_cost(x_target, x_sim, w_p, precision_matrix, dt, interval=(0, None)):
     """Summed squared difference between target and simulation within specified time interval weighted by w_p.
 
     :param x_target:    N x V x T array that contains the target time series.
@@ -15,9 +15,12 @@ def precision_cost(x_target, x_sim, w_p, precision_matrix, interval=(0, None)):
     :param w_p:         Weight that is multiplied with the precision cost.
     :type w_p:          float
 
-    :param precision_matrix: N x V binary matrix that defines nodes and channels of precision measurement, defaults to
-                                 None
+    :param precision_matrix: N x V binary matrix that defines nodes and channels of precision measurement. Defaults to
+                                 None.
     :type precision_matrix:  np.ndarray
+
+    :param dt:  Time step.
+    :type dt:   float
 
     :param interval:    (t_start, t_end). Indices of start and end point of the slice (both inclusive) in time
                         dimension. Default is full time series, defaults to (0, None).
@@ -37,7 +40,7 @@ def precision_cost(x_target, x_sim, w_p, precision_matrix, interval=(0, None)):
             for t in range(interval[0], interval[1]):
                 cost += precision_matrix[n, v] * (x_target[n, v, t] - x_sim[n, v, t]) ** 2
 
-    return w_p * 0.5 * cost
+    return w_p * 0.5 * cost * dt
 
 
 @numba.njit
@@ -78,7 +81,7 @@ def derivative_precision_cost(x_target, x_sim, w_p, precision_matrix, interval=(
 
 
 @numba.njit
-def energy_cost(u, w_2):
+def energy_cost(u, w_2, dt):
     """
     :param u:   Control-dimensions x T array. Control signals.
     :type u:    np.ndarray
@@ -86,10 +89,13 @@ def energy_cost(u, w_2):
     :param w_2: Weight that is multiplied with the W2 ("energy") cost.
     :type w_2:  float
 
+    :param dt:  Time step.
+    :type dt:   float
+
     :return:    W2 cost of the control.
     :rtype:     float
     """
-    return w_2 * 0.5 * np.sum(u**2.0)
+    return w_2 * 0.5 * np.sum(u**2.0) * dt
 
 
 @numba.njit

neurolib/optimal_control/tests/test_cost_functions.py
@@ -15,45 +15,47 @@ class TestCostFunctions(unittest.TestCase):
         w_p = 1
         N = 1
         precision_cost_matrix = np.ones((N, 2))
+        dt = 0.1
         x_target = self.get_arbitrary_array()
 
         self.assertEqual(
-            cost_functions.precision_cost(x_target, x_target, w_p, precision_cost_matrix),
+            cost_functions.precision_cost(x_target, x_target, w_p, precision_cost_matrix, dt),
             0,
         )  # target and simulation coincide
 
         x_sim = np.copy(x_target)
         x_sim[:, 0] = -x_sim[:, 0]  # create setting where result depends only on this first entries
         self.assertEqual(
-            cost_functions.precision_cost(x_target, x_target, w_p, precision_cost_matrix),
+            cost_functions.precision_cost(x_target, x_target, w_p, precision_cost_matrix, dt),
             0,
         )
         self.assertEqual(
-            cost_functions.precision_cost(x_target, x_sim, w_p, precision_cost_matrix),
-            w_p / 2 * np.sum((2 * x_target[:, 0]) ** 2),
+            cost_functions.precision_cost(x_target, x_sim, w_p, precision_cost_matrix, dt),
+            w_p / 2 * np.sum((2 * x_target[:, 0]) ** 2) * dt,
         )
 
     def test_precision_cost_nodes_channels(self):
-        print(" Test precision cost full timeseries for node and channel selction")
+        print(" Test precision cost full timeseries for node and channel selection.")
         w_p = 1
         N = 2
         x_target0 = self.get_arbitrary_array()
         x_target1 = 2.0 * self.get_arbitrary_array()
         target = np.concatenate((x_target0, x_target1), axis=0)
         precision_cost_matrix = np.zeros((N, 2))
+        dt = 0.1
         zerostate = np.zeros((target.shape))
 
         self.assertEqual(
-            cost_functions.precision_cost(target, zerostate, w_p, precision_cost_matrix),
+            cost_functions.precision_cost(target, zerostate, w_p, precision_cost_matrix, dt),
             0.0,
         )  # no cost if precision matrix is zero
 
         for i in range(N):
             for j in range(N):
                 precision_cost_matrix[i, j] = 1
-                result = w_p * 0.5 * sum((target[i, j, :] ** 2))
+                result = w_p * 0.5 * sum((target[i, j, :] ** 2)) * dt
                 self.assertEqual(
-                    cost_functions.precision_cost(target, zerostate, w_p, precision_cost_matrix),
+                    cost_functions.precision_cost(target, zerostate, w_p, precision_cost_matrix, dt),
                     result,
                 )
                 precision_cost_matrix[i, j] = 0
@@ -103,13 +105,16 @@ class TestCostFunctions(unittest.TestCase):
         w_p = 1
         N = 1
         precision_cost_matrix = np.ones((N, 2))
+        dt = 0.1
         x_target = np.concatenate((self.get_arbitrary_array(), self.get_arbitrary_array()), axis=2)
         x_sim = np.copy(x_target)
         x_sim[0, :, 3] = -x_sim[0, :, 3]
         interval = (3, None)
-        precision_cost = cost_functions.precision_cost(x_target, x_sim, w_p, precision_cost_matrix, interval)
+        precision_cost = cost_functions.precision_cost(
+            x_target, x_sim, w_p, precision_cost_matrix, dt, interval=interval
+        )
         # Result should only depend on second half of the timeseries.
-        self.assertEqual(precision_cost, w_p / 2 * np.sum((2 * x_target[0, :, 3]) ** 2))
+        self.assertEqual(precision_cost, w_p / 2 * np.sum((2 * x_target[0, :, 3]) ** 2) * dt)
 
     def test_derivative_precision_cost_in_interval(self):
         """This test is analogous to the 'test_derivative_precision_cost'. However, the signal is repeated twice, but
@@ -131,10 +136,11 @@ class TestCostFunctions(unittest.TestCase):
 
     def test_energy_cost(self):
         print(" Test energy cost")
-        reference_result = 112.484456945
+        dt = 0.1
+        reference_result = 112.484456945 * dt
         w_2 = 1
         u = self.get_arbitrary_array()
-        energy_cost = cost_functions.energy_cost(u, w_2)
+        energy_cost = cost_functions.energy_cost(u, w_2, dt)
         self.assertEqual(energy_cost, reference_result)
 
     def test_derivative_energy_cost(self):