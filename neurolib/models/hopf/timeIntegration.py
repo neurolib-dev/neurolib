cffa710c75aac90b6ef1205d956b6582459b3f03
Lena Salfenmoser <lena.salfenmoser@tu-berlin.de>
2024-01-05 09:33:14 +0100

    fix style hopf

--- neurolib/models/hopf/timeIntegration.py
+++ neurolib/models/hopf/timeIntegration.py
@@ -190,7 +190,7 @@ def timeIntegration_njit_elementwise(
             # diffusive coupling
             if coupling == 0:
                 for l in range(N):
-                    xs_input_d[no] += xs_input_d[no] += K_gl * Cmat[no, l] * (xs[l, i - Dmat_ndt[no, l] - 1] - xs[no, i - 1])
+                    xs_input_d[no] += K_gl * Cmat[no, l] * (xs[l, i - Dmat_ndt[no, l] - 1] - xs[no, i - 1])
                     # ys_input_d[no] += K_gl * Cmat[no, l] * (ys[l, i - Dmat_ndt[no, l] - 1] - ys[no, i - 1])
             # additive coupling
             elif coupling == 1:
